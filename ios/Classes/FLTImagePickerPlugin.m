3187488281c244506d01c17dfa74194f07573042
Steven Gunanto <Steven.Gunanto@rga.com>
2020-11-20 09:47:11 +0700

    Change modal presentation style

--- ios/Classes/FLTImagePickerPlugin.m
+++ ios/Classes/FLTImagePickerPlugin.m
@@ -69,7 +69,7 @@ static const int SOURCE_GALLERY = 1;
 
   if ([@"pickImage" isEqualToString:call.method]) {
     _imagePickerController = [[UIImagePickerController alloc] init];
-    _imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    _imagePickerController.modalPresentationStyle = UIModalPresentationOverCurrentContext;
     _imagePickerController.delegate = self;
     _imagePickerController.mediaTypes = @[ (NSString *)kUTTypeImage ];
 
